3d3f95009fab9a7cb74d4fe118eb00bd15709cf8
Otter <tanakajanaidayo@gmail.com>
2025-07-22 09:20:55 +0200

    Add files via upload

--- otterstatus.c
+++ otterstatus.c
@@ -14,6 +14,7 @@
 #include <X11/XKBlib.h>
 #include <X11/Xlib.h>
 
+#define MAX_BUF_SIZE 47
 // volume defines
 #define MIXER_DEVICE "/dev/mixer"
 #define MIXER_VOL MIXER_READ(SOUND_MIXER_VOLUME)
@@ -23,7 +24,7 @@
 Display *dpy;
 struct timespec toSleep;
 
-char buf[48];
+char buf[MAX_BUF_SIZE+1];
 
 void dateTime(void)
 {
@@ -32,7 +33,7 @@ void dateTime(void)
 	char result[24];
 	if(!strftime(result, sizeof(result), "%m月%d日(%a) %H:%M", localtime(&t))) return;
 	
-	sprintf(buf, "%s %s", buf, result);
+	snprintf(buf, MAX_BUF_SIZE, "%s %s", buf, result);
 }
 
 void volume(void)
@@ -40,38 +41,36 @@ void volume(void)
 	int v, afd;
 
 	if((afd = open(MIXER_DEVICE, O_RDONLY | O_NONBLOCK)) < 0) return;
-
 	if(ioctl(afd, MIXER_VOL, &v) < 0)
 	{
 		close(afd);
 		return;
 	}
 
-	sprintf(buf, "%s VOL %3d%%", buf, v & 0xFF);
+	snprintf(buf, MAX_BUF_SIZE, "%s VOL %3d%%", buf, v & 0xFF);
 	close(afd);
 }
 
 void battery(void)
 {
 	int result;
-
 	size_t len = sizeof(result);
-	if(sysctlbyname("hw.acpi.battery.life", &result, &len, NULL, 0) == -1) return;
 
+	if(sysctlbyname("hw.acpi.battery.life", &result, &len, NULL, 0) == -1) return;
 	if(result < 0) return;
-	sprintf(buf, "%s BAT %3d%%", buf, result);
+
+	snprintf(buf, MAX_BUF_SIZE, "%s BAT %3d%%", buf, result);
 }
 
 void keymap(void)
 {
-	char result[4];
 	XkbStateRec state;
 
 	if(XkbGetState(dpy, XkbUseCoreKbd, &state)) return;
 	XkbDescPtr desc = XkbGetKeyboard(dpy, XkbAllComponentsMask, XkbUseCoreKbd);
 
-	snprintf(result, 3, "%s", KEYMAP_GROUP);
-  sprintf(buf, "%s [%s]", buf, result); 
+	//snprintf(result, 3, "%s", KEYMAP_GROUP);
+  snprintf(buf, MAX_BUF_SIZE, "%s [%.2s]", buf, KEYMAP_GROUP); 
 }
 
 void updatestatus(void)
